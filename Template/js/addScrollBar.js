fb6effeb6450f771fdc74224148f2e9fd918274f
PB17111642 <2439758713@qq.com>
2019-09-25 17:29:20 +0800

    add autoMenu and scrollBar script in /Team/TEAM/Collaboration.html

---
+++ Template/js/addScrollBar.js
@@ -0,0 +1,32 @@
+$(document).ready(function () {
+    // add the div we want to use
+    $('body').append('<div class="scroll position-fixed" style="width:70px; bottom: 100%;right:1em; transition: all .3s ease-in-out">' +
+        '<img src="../../File/scroll.png"/>' +
+        '<div class="position-relative backHead" style="width: 70px; height: 110px; top: -110px; cursor: pointer;"></div>' +
+        '</div>');
+
+    // basic operation
+    let show = false;
+    $(window).scroll(function () {
+        let toTop = $(document).scrollTop();
+        if (toTop > 300 && !show){
+            $(".scroll").css("bottom", "5%");
+            console.log('gt 300px');
+            show = true;
+        }else if(toTop <= 300 && show){
+            $(".scroll").css("bottom", "100%");
+            console.log('lt 300px');
+            show = false;
+        }
+    });
+
+    // make the img clickable
+    $('.backHead').on('click', function () {
+        $('body, html').animate({
+            scrollTop: 0,
+            screenLeft: 0
+        }, 300);
+        $(".scroll").css("bottom", "100%");
+    });
+
+});
